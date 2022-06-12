817154184d6132b601d08f25813b61acfe19d5bb
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-06-12 17:49:18 +0100

    make it better

--- middlware/auth-middlware.js
+++ middlware/auth-middlware.js
@@ -1,13 +1,13 @@
 //midllwares are used to check if the user is logged in or not
 //and if not logged in then redirect to login page
 const jwt = require("jsonwebtoken");
-const jwtSecret =JWT_SECRET_PROD ||"secret";
+const JWT_SECRET = process.env.JWT_SECRET_PROD || "secret";
 exports.requireLogin = (req, res, next) => {
   console.log("req.headers.authorization:", req.headers.authorization);
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, jwtSecret, (err, decoded) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).json({ error: "Unauthorized" });
       }
