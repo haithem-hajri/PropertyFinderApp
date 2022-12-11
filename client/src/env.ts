16af122554b5efabf87f97ce64f0e509a934382c
haithem711 <49453214+haithem711@users.noreply.github.com>
2022-12-11 17:47:13 +0100

    fix ui

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,4 +1,4 @@
 //export const API_URL = https://cherchi.herokuapp.com/api ;
-export const API_URL = "https://cherchi.herokuapp.com/api"
+export const API_URL = "https://cherchi-api.onrender.com/api"
 // ||"http://localhost:5000/api";
 
